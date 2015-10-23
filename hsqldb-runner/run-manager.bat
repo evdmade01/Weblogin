f383b7b3d44517da17447374b5c65efa24550ae6
Erik van der Made <erik.van.der.made@cerios.nl>
2015-10-23 15:15:18 +0200

    publish

--- hsqldb-runner/run-manager.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set HOME=c:\
-set JAVA_HOME=C:\Demo\jdk_1.7.0_21
-set M2_HOME=C:\Demo\maven_3.0.5
-set M3_HOME=C:\Demo\maven_3.0.5
-set PATH="%JAVA_HOME%\bin";"%M3_HOME%\bin";%PATH%
-mvn exec:java -Dexec.mainClass="org.hsqldb.util.DatabaseManagerSwing" -Dexec.args="--url jdbc:hsqldb:hsql://localhost/"
